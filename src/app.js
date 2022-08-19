214fea933c5b67db630b69220f2ab59146327a31
Denis <d.combe@laposte.net>
2022-08-19 18:06:29 +0200

    acces un seul pay par /covid/Pays - fin

--- src/app.js
+++ src/app.js
@@ -10,14 +10,33 @@ const fetchCovidData = require("./scrapper");
 
 const app = express();
 
+let cacheTime; // permet d'éviter de se faire blacklister par le site à cause du nombre de requêtes
+let data;
+
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 app.get("/covid", async (req, res) => {
-  const data = await fetchCovidData();
+  if (cacheTime && cacheTime > Date.now() - 1000 * 30) {
+    // cache de 30 sec
+    return res.json(data);
+  }
+  data = await fetchCovidData();
+  cacheTime = Date.now();
   res.json(data);
+});
+
+app.get("/covid/:country", async (req, res) => {
+  if (!data) {
+    data = await fetchCovidData();
+  }
+  res.json(data.filter((d) => d.country === req.params.country));
+});
+
+app.get("/", async (req, res) => {
+  res.redirect("/covid");
 });
 
 app.use(middlewares.notFound);
